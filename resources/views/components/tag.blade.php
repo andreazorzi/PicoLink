079234cff8bbfd493854bad5baae0e5652dabd65
Andrea Zorzi <andreaz@europlan.it>
2024-06-26 16:10:49 +0200

    Update tag ui

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,4 @@
-<span class="badge fs-6_5 me-2" style="background-color: {{$tag->background_color ?? "#000000"}}; color: {{$tag->text_color ?? "#ffffff"}};" role="button"
+<span class="badge fs-6_5 me-2 mb-2" style="background-color: {{$tag->background_color ?? "#000000"}}; color: {{$tag->text_color ?? "#ffffff"}};" role="button"
 	@if($editable ?? false)
 		hx-post="{{route("tag.details", [$tag])}}" hx-target="#modal .modal-content"
 	@endif
